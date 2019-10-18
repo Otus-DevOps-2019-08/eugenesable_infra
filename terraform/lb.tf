7edc025f14c5da19c03707de72ab5e689bf527a3
eugenesable <jkennedy226@gmail.com>
2019-10-18 16:40:50 +0300

    modofied with valid names

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,22 +1,20 @@
-resource "google_compute_forwarding_rule" "lb-fw" {
+resource "google_compute_target_pool" "loadbalancer" {
+  name          = "reddit-loadbalancer"
+  instances     = "${google_compute_instance.app.*.self_link}"
+  health_checks = ["${google_compute_http_health_check.healthcheck.name}"]
+}
+
+resource "google_compute_forwarding_rule" "loadbalancer-firewall" {
   name                  = "reddit-app"
-  description           = "Forwarding rule for Reddit apps"
   port_range            = "9292"
-  target                = "${google_compute_target_pool.lb.self_link}"
+  target                = "${google_compute_target_pool.loadbalancer.self_link}"
   load_balancing_scheme = "EXTERNAL"
 }
 
-resource "google_compute_target_pool" "lb" {
-  name          = "reddit-lb"
-  description   = "Load balancer for Reddit apps"
-  instances     = "${google_compute_instance.app.*.self_link}"
-  health_checks = ["${google_compute_http_health_check.default.name}"]
-}
-
-resource "google_compute_http_health_check" "default" {
-  name                = "reddit-app-hc"
+resource "google_compute_http_health_check" "healthcheck" {
+  name                = "healthcheck"
   port                = 9292
-  check_interval_sec  = 10
-  timeout_sec         = 5
-  unhealthy_threshold = 5
+  request_path       = "/"
+  check_interval_sec  = 1
+  timeout_sec         = 1
 }
