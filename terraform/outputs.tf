3cadf237803b8361ea6f66c7c6938fd057ae96e8
eugenesable <jkennedy226@gmail.com>
2019-10-18 16:42:04 +0300

    loadbalancer resource.name modified

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "app_external_ip" {
-  value = google_compute_instance.app.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.app[*].network_interface[0].access_config[0].nat_ip
 }
 
 output "lb_external_ip" {
-  value = google_compute_forwarding_rule.lb-fw.ip_address
+  value = google_compute_forwarding_rule.loadbalancer-firewall.ip_address
 }
